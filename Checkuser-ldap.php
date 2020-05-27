6596cd1147028416d63893ec486aaccdbe36fb2d
John Mark <johnmarkroco05@gmail.com>
2020-05-27 11:12:40 +0800

    Update Checkuser-ldap.php

--- Checkuser-ldap.php
+++ Checkuser-ldap.php
@@ -4,16 +4,19 @@
    $ldapport = 389;
    $ldappass =  '';
    $username = '';
-   $ldapdn = "uid=".$username.",o=University of the Philippines,c=PH";
+   $ldapdn = "uid=".$username.",o=University of the Quezon City,c=PH";
    $ldapconn = @ldap_connect($ldaphost, $ldapport) or die("Could not connect to $ldaphost");
    ldap_set_option($ldapconn, LDAP_OPT_PROTOCOL_VERSION, 3);
    @ldap_bind($ldapconn, $ldapdn, $ldappass);
    $filter = '(uid=sampleUsername)';
-   $resldap = ldap_search($ldapconn, 'o=University of the Philippines,c=PH', $filter);
+   $resldap = ldap_search($ldapconn, 'o=University of the Quezon City,c=PH', $filter);
    $info = ldap_get_entries($ldapconn, $resldap);
-   print "<pre>";
+   for($i = 0; $i < $info['count']; $i++){
+        echo $info[$i]['employeetype'][0];
+   }
+   /*print "<pre>";
    print_r($info);
-   print "</pre>";
+   print "</pre>";*/
    exit;
 ?>
 ```
